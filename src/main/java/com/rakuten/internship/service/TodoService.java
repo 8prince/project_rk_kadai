97fe2db63c9f522c99d791f92cc3a540a221b0a1
Kazuki Fujioka <fujioka@ai.cs.kobe-u.ac.jp>
2019-09-09 13:08:45 +0000

    use jpa

--- src/main/java/com/rakuten/internship/service/TodoService.java
+++ src/main/java/com/rakuten/internship/service/TodoService.java
@@ -1,10 +1,12 @@
 package com.rakuten.internship.service;
 
 import com.rakuten.internship.entity.Todo;
+import com.rakuten.internship.repository.TodoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,48 +22,50 @@ public class TodoService {
 
     // TODO 必要なメンバーを宣言してください。
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private TodoRepository repository;
 
     public List<Todo> findTodos(){
         // TODO 必要なコードを作成してください。
-        List list = jdbcTemplate.queryForList("select * from tasks");
+        //List list = jdbcTemplate.queryForList("select * from tasks");
+        List list = repository.findAll();
         
-        List<Todo> todos = new ArrayList<Todo>();
-        for (Iterator it = list.iterator(); it.hasNext();) {
-            Map map = (Map)it.next();
-            Todo todo = createTodo(map);
-            todos.add(todo);
-        }
+        //List<Todo> todos = new ArrayList<Todo>();
+        //for (Iterator it = list.iterator(); it.hasNext();) {
+        //    Map map = (Map)it.next();
+        //    Todo todo = createTodo(map);
+        //    todos.add(todo);
+        //}
                 
-        return todos;
+        return list;
     }
     
     // try ～ catch 使えばいけそうな気もするが…
-    private Todo createTodo(Map map) {
-        Todo todo = new Todo();
-        todo.setId(((Integer) map.get("number")).intValue());
-        todo.setTitle((String) map.get("title"));
-        todo.setDescription((String) map.get("description"));
-        return todo;
-    }
+    //private Todo createTodo(Map map) {
+    //    Todo todo = new Todo();
+    //    todo.setId(((Integer) map.get("number")).intValue());
+    //    todo.setTitle((String) map.get("title"));
+    //    todo.setDescription((String) map.get("description"));
+    //    return todo;
+    //}
     
     public Todo save(Todo todo) {
         // TODO 必要なコードを作成してください。
-        String sql = "insert into tasks(number, title, description) values("
-            + todo.getId()
-            + ",'" 
-            + todo.getTitle()
-            + "','"
-            + todo.getDescription()
-            + "');";
-        jdbcTemplate.update(sql);        
+        //String sql = "insert into tasks(number, title, description) values("
+        //    + todo.getId()
+        //    + ",'" 
+        //    + todo.getTitle()
+        //    + "','"
+        //    + todo.getDescription()
+        //    + "');";
+        //jdbcTemplate.update(sql);        
         
-        return null;
+        return repository.save(todo);
     }
 
 
     public int returnRecords(){
-        List list = jdbcTemplate.queryForList("select * from tasks");
+        //List list = jdbcTemplate.queryForList("select * from tasks");
+        List list = repository.findAll();
         return list.size();
     }
 }
